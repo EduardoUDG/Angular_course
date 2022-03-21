961f1aab34bb6a648f02466da5b600b73d537d5e
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-03-21 11:02:02 -0600

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
 
+interface Person {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -8,8 +13,16 @@ import { Component } from '@angular/core';
 export class MainPageComponent {
 
 
-  addPerson() {
-    console.log('Hey!!');
+  person:Person = {
+    name: 'Trucks',
+    power: 14000
   }
 
+  addPerson() {
+    console.log( this.person );
+  }
+
+  // cambiarNombre( event:any ) {
+  //   console.log( event.target.value );
+  // }
 }
