2474b370d9906b21c9660cd8acda9c36c9e8d97c
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-03-20 18:06:32 -0600

    *ngFor

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+})
+export class ListComponent {
+
+  heroes      : string[] = ['Spiderman', 'Iroman', 'Hulk', 'Thor'];
+  heroDeleted : string = '';
+
+  deleteHero():void {
+    this.heroDeleted = this.heroes.shift() || '';
+
+  }
+
+  constructor() { }
+
+}
